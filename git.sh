41d797ff21439c6076e8c919f403e4cb70e1b144
sandakith <sandakith@gmail.com>
2014-07-15 13:38:47 -0400

    adding git script

---
+++ git.sh
@@ -0,0 +1,21 @@
+## git sample commands 
+# git clone https://github.com/sandakith/SandBox.git
+# git status
+# git add README.md 
+# git reset HEAD <Folder> 
+# git checkout README.md
+# git commit -m "Update Readme"
+# git status
+# git diff
+# git push origin master 
+
+#git log origin/master..HEAD
+#git diff origin/master..HEAD
+
+
+git config --global user.name "sandakith"
+git config --global user.email "sandakith@gmail.com"
+
+echo "Updating git settings"
+git config user.name
+git config user.email
