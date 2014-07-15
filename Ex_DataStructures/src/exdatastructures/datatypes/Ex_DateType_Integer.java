404b049f3662bf6f1e20ffd281e421aa79afa1fa
sandakith <sandakith@gmail.com>
2014-07-15 11:03:10 -0400

    ading Integer Test

---
+++ Ex_DataStructures/src/exdatastructures/datatypes/Ex_DateType_Integer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exdatastructures.datatypes;
+
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ex_DateType_Integer {
+
+
+    public static void main(String[] args) {
+    	
+    	// Get the current time in long and convert to readable format 
+    	
+    	// Integer Upper Bound: 2147483647 (2^31 -1)
+    	final int upperBound = (int)(Math.pow(2, 33)-1);
+    	System.out.println("Integer Upper Bound: "+ upperBound);
+    	
+    }
+		
+
+}
